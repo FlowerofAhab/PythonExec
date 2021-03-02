9eff98ae497944e7c75d615e48ed5f593a1eff9c
FlowerofAhab <78939038+FlowerofAhab@users.noreply.github.com>
2021-03-02 10:06:23 -0300

    Add files via upload

---
+++ repetição.py
@@ -0,0 +1,10 @@
+while True:
+    n = int(input('Insira a nota de 0 a 10: '))
+    if n < 0 or n > 10:
+        print ('valor inválido')
+    if n >=0 and n <=10:
+        print (f'sua nota é {n}')
+        break
+        
+
+        
